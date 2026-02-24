17284204c8ae62ae0e275e201eb4820e1e2a72e7
RachelBancoArbi <rachel.silva@bancoarbi.com.br>
2026-02-24 17:29:50 -0300

    dep: installs resend and react-email components, to start developing email verification before auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,12 +1,28 @@
 import { Auth, betterAuth, BetterAuthOptions } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
+import {Resend} from "resend";
+import VerifyEmail from "@/components/ui/emails/verify-email";
+
+const resend:Resend = new Resend(process.env.RESEND_API_KEY as string);
 
 export const auth : Auth<BetterAuthOptions>= betterAuth({
     database: prismaAdapter(prisma, {
         provider: "postgresql", // or "mysql", "postgresql", ...etc
     }), 
     emailAndPassword: {
-        enabled: true,
+        enabled: true, 
+        requireEmailVerification: true,
+    },
+    emailVerification: {
+        sendVerificationEmail: async ({ user, url }) => {
+            await resend.emails.send({
+                from: `${process.env.EMAIL_SENDER_NAME} <${process.env.EMAIL_SENDER_ADDRESS}>`,
+                to: user.email,
+                subject: "Verify your email",
+                react: VerifyEmail({ username: user.name, verifyUrl: url }),
+            });
+        },
+        sendOnSignUp: true,
     }
 });

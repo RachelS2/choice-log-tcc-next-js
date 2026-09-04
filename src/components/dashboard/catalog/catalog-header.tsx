e2c461ee6f4037acb4ab398176bb180adacd6dfd
RachelS2 <rachelbarinosilva@gmail.com>
2026-09-04 16:45:50 -0300

    feat: refactor CatalogHeader to use PageHeader and PageTitle components for improved structure

--- src/components/dashboard/catalog/catalog-header.tsx
+++ src/components/dashboard/catalog/catalog-header.tsx
@@ -1,6 +1,7 @@
 import { Plus } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { CategoryModel } from '@/models/dashboard/items';
+import { PageHeader, PageTitle } from '@/components/ui/pages-title';
 
 interface CatalogHeaderProps {
   onNewItem: () => void;
@@ -8,32 +9,11 @@ interface CatalogHeaderProps {
 }
 export default function CatalogHeader({ onNewItem, newItemBtnDisabled }: CatalogHeaderProps) {
   return (
-    <div className="flex flex-col gap-4 mx-auto w-full max-w-7xl sm:flex-row sm:items-center sm:justify-between">
+    <div className="flex flex-col gap-2 mx-auto w-full max-w-7xl sm:flex-row sm:items-center sm:justify-between">
       <div className="flex flex-col">
-        <div className="mb-3 flex items-center gap-2.5">
-          <span className="h-px w-6 bg-blue-900/70" />
-
-          <span className="text-[10px] font-semibold tracking-[0.22em] text-blue-900 uppercase">
-            Navegue e gerencie todos os produtos e serviços que você registrou.
-          </span>
-        </div>
-
-        <h1
-          className="
-      font-[family-name:var(--font-inter)]
-      text-3xl
-      font-semibold
-      leading-tight
-      tracking-[-0.02em]
-      text-blue-800
-      sm:text-4xl
-    "
-        >
-          Seus{" "}
-          <span className="text-blue-900/90">
-            Itens
-          </span>
-        </h1>
+          <PageHeader header="Navegue e gerencie todos os produtos e serviços que você registrou." lineBefore/>
+          
+          <PageTitle title="Seus Itens" />
 
       </div>
       <Button
